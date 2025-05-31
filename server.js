a1ae71e03bf3e16c062a744000d0dc245ddcad16
Andrew <abulfayz7@gmail.com>
2025-05-31 18:51:45 +0900

    BRR: build express web server

---
+++ server.js
@@ -0,0 +1,29 @@
+console.log("Start Web Server");
+const express = require('express');
+const app = express();
+const http = require("http");
+
+// 1 - Intro codes: Codes that are related to the data coming to express 
+app.use(express.static("public"));                  // any requests from browser can only access this folder (CSS, images etc.)
+app.use(express.json());                            // will convert json data to an object data. We know the data between client and web server is in json format
+app.use(express.urlencoded({extended: true}));      // traditional request form. By writing this code our express will accept any post from html FORM
+
+
+// 2 - Session codes
+// 3 - Views codes
+app.set("views", "views");          // 2nd argument is a folder name
+app.set("view engine", "ejs");
+
+// 4 - Routing codes
+app.get("/hello", function(req, res) {
+    res.end(`<h1 style="background: red">HELLO WORLD by Andrew</h1>`);
+});
+app.get("/gift", function(req, res) {
+    res.end(`<h1>You are in gifts section</h1>`);
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function() {
+    console.log(`The server is running successfully on port: ${PORT}`);
+});
---
+++ train.js
@@ -0,0 +1,7 @@
+console.log("TRAIN AREA!");
+
+// A-TASK
+
+
+
+
