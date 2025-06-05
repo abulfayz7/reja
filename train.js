cb9115cab3aad64091f82148e667f1261caac19f
Andrew <abulfayz7@gmail.com>
2025-06-05 20:58:53 +0900

    BRR:feat: train.js A-TASK complete

--- train.js
+++ train.js
@@ -1,7 +1,130 @@
 console.log("TRAIN AREA!");
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yahshi talaba buling", // 0-20
+//     "togri boshliq tanlang va koproq hato qiling", // 20-30
+//     "uzingizga ishlashingizni boshlang", // 30-40
+//     "siz kuchli bolgan narsalarni qiling", // 40-50
+//     "yoshlarga investitsiya qiling", // 50-60
+//     "endi dam oling, foydasi yoq endi" // 60
+// ];
+
+// --------------------- CALLBACK FUNCTION --------------------- //
+//  function giveMeAdvice(a, callback) {
+//     if (typeof a !== 'number') callback("insert a number", null);
+//     else if (a <= 20) callback(null, list[0]);
+//     else if (a > 20 && a <= 30) callback(null, list[1]);
+//     else if (a > 30 && a <= 40) callback(null, list[2]);
+//     else if (a > 40 && a <= 50) callback(null, list[3]);
+//     else if (a > 50 && a <= 60) callback(null, list[4]);
+//     else {
+//         setInterval(() => {                    // Unlike async functions, callback function will continously print the data
+//             callback(null, list[5]);
+//         }, 1000);
+//         // setTimeout(function() {
+//         //     callback(null, list[5]);
+//         // }, 5000);
+//     }
+// }
+    
+// giveMeAdvice(65, (err, data) => {
+//     if (err) console.log('ERROR', err);
+//     else { 
+//         console.log('Advice:', data);
+//     }
+// }); 
+
+// ------------------- ASYNCHRONOUS FUNCTION ------------------- //
+// async function giveMeAdvice(a) {            // Asynchronous function definition
+//     if (typeof a !== 'number') throw new Error("insert a number");
+//     else if (a <= 20) return list[0];
+//     else if (a > 20 && a <= 30) return list[1];
+//     else if (a > 30 && a <= 40) return list[2];
+//     else if (a > 40 && a <= 50) return list[3];
+//     else if (a > 50 && a <= 60) return list[4];
+//     else {
+//         // return list[5];
+//         // setTimeout(function() {
+//         //     return list[5];
+//         // }, 5000);
+
+//         // We can't use setTimeout() inside async function directly so we use it with Promise()
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {                           // If we use serInterval(funct, time)
+//             resolve(list[5]);                            // which loops the logic, async function
+//         }, 5000);                                        // will just run it once and stop(wait).
+//         });
+//     }
+// }
+
+// console.log('passed here 0');
+
+// -------------------------- Call via then() and catch() --------------------------
+// giveMeAdvice(25)
+//     .then((data) => {
+//         console.log('Advice:', data);
+//     })
+//     .catch((err) => {
+//         console.log('ERROR', err);
+//     });
+
+// ------------- Using asynchronous function and await in call part too -------------
+// async function run() {
+//     let answer = await giveMeAdvice(25);
+//     console.log('Advice:', answer);
+//     answer = await giveMeAdvice(70);
+//     console.log('Advice:', answer);
+//     answer = await giveMeAdvice(41);
+//     console.log('Advice:', answer);
+// }
+
+// run();
+
+// console.log('passed here 1'); 
+
+
+
+
+
+
+
+
 
 // A-TASK
 
+/* Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+MASALAN countLetter("e", "engineer") 3ni return qiladi.
 
+masalani yechimi: */
+
+const countLetter = (letter, string) => {
+    let count = 0;
+    for (const character of string) {
+        if (character === letter) {
+        count++;
+        }
+    }
+    return count;
+}
+
+console.log("Letters in string:", countLetter("a", "andrew (abulfayz)"));
+
+
+// Additional practice
+
+const countLetterInArray = (letter, array) => {
+    let countArr = 0;
+    for (const ele of array) {
+        for (const character of ele) {
+            if (character === letter) {
+                countArr++;
+            }
+        }
+    }
+    return countArr;
+}
+
+const arr = ["andrew", "abulfayz", "async", "fullstack"];
+console.log("Letters in Array:", countLetterInArray("a", arr));
 
 
